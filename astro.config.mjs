48f1b61a9c181bb0b3ac3c64f8e3ce9eaced34d9
CodeWithKyle <kyleanicholson@gmail.com>
2023-10-24 21:19:45 -0700

    Fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https:/kylenicholson.dev",
+  site: "https://kylenicholson.dev",
 });
